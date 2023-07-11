702fd5131d61f2dcdfd33797a4177e9988d3598d
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-07-10 23:26:13 -0500

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DIR=$(pwd)/build
+if [ -d "$DIR" ]; then
+  printf '%s\n' "Removing Lock ($DIR)"
+  rm -rf "$DIR"
+fi
+
+BRAID_DIR=/usr/include
+
+cmake \
+  -S code-gallery/parallel_in_time \
+  -B build \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_STANDARD=17 \
+  -Wno-dev
+
+cmake --build build
