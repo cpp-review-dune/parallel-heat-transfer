c9204526e5d18e900419e3043617636b97458adb
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-07-10 23:39:56 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,13 @@
 DIR=$(pwd)/build
 if [ -d "$DIR" ]; then
   printf '%s\n' "Removing Lock ($DIR)"
-  rm -rf "$DIR"
+  rm -rf "$DIR" *.tar.zst
 fi
 
 export BRAID_DIR=/usr/include/xbraid/
+# export OMP_PROC_BIND=true
+
+TIME=$(date -u +"%Y-%m-%d-%H-%M-%S" --date='5 hours ago')
 
 cmake \
   -S code-gallery/parallel_in_time \
@@ -17,5 +20,6 @@ cmake \
 
 cmake --build build
 pushd build
-./parallel_in_time | tee -a $(date -u +"%Y-%m-%d-%H-%M-%S" --date='5 hours ago').log
+./parallel_in_time | tee -a $TIME.log
 popd
+tar -c -I 'zstd -19 -T0' -f $TIME.tar.zst build
